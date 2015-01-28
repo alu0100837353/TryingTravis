2c2d89328da96d30973cd32d84c309aaeb655724
Victor Hernandez Perez <alu0100697032.edu.es>
2015-01-28 23:27:57 +0000

    Style changes, modified regular expresion in temperature.js and alerts to the user implemented

--- temperature.js
+++ temperature.js
@@ -1,10 +1,21 @@
 function calcular_temperatura(){
     
-    var temperatura = temperatura_original.value;
+    var temperatura = temperatura_original.value;//guardamos el valor de la temperatura qe ha insertado el usuario
     
+    /*
+    * Comprobamos que el valor introducido tiene el formato correcto, si no se muestra una alerta 
+    */
     if(temperatura.match(/[+-]?\d+((\.)\d+)?\s*[CcFf]/)){
+        
+        temperatura_convertida.className = "alert alert-success"
+        temperatura_convertida.style.display = "block"
         temperatura_convertida.innerHTML = "Valor correcto";
+        
     }else{
-        temperatura_convertida.innerHTML = "Valor incorrecto";
+        
+        temperatura_convertida.className = "alert alert-danger"
+        temperatura_convertida.style.display = "block"
+        temperatura_convertida.innerHTML = "Error en los datos de entrada. Pruebe algo cómo se indica en el formato.";
+   
     }
 }
