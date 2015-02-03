1d3f941737861a1bfc33b570b99d888adc3fc23b
Victor Hernandez Perez <alu0100697032.edu.es>
2015-02-03 20:34:13 +0000

    temperature.js changes: Celcius to all types supported

--- temperature.js
+++ temperature.js
@@ -10,15 +10,43 @@ function calcular_temperatura(){
     if(matching){
         var numero = matching[1];
         var tipo_grado = matching[2];
-        
+        var tipo_conversion = $('input[name=grados]:checked').val();
+        numero = parseFloat(numero);
         switch (tipo_grado) {
-            case 'c' || 'C':
-                numero = numero * 1.8 + 32;
-                mensaje_mostrado = numero + "º Farenheit";
+            //De celcius a...
+            case 'c':
+            case 'C':
+                switch (tipo_conversion) {
+                    //farenheit
+                    case 'f':
+                        numero = numero * 1.8 + 32;
+                        mensaje_mostrado = numero + "º Farenheit";
+                        break;
+                        //celcius
+                    case 'c':
+                        mensaje_mostrado = numero + "º Celcius";
+                        break;
+                        //kelvin
+                    case 'k':
+                        numero = numero + 273.15;
+                        mensaje_mostrado = numero + "º Kelvin";
+                        break;  
+                    //rankine
+                    case 'ra':
+                        numero = (numero * 9)/5 + 491.67;
+                        mensaje_mostrado = numero + "º Rankine";
+                        break;
+                    //reaumur
+                    case 're':
+                        numero = (numero * 4)/5;
+                        mensaje_mostrado = numero + "º Réaumur";
+                        break;    
+                    default:
+                        mensaje_mostrado = "Elija algún tipo al que covertir";
+                }
                 break;
             
             default:
-                // code
         }
         temperatura_convertida.className = "alert alert-success"
         temperatura_convertida.style.display = "block"
