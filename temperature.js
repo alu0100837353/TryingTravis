f24a61179a6c30c7a5a1bb8940d25389a50fe14d
Victor Hernandez Perez <alu0100697032.edu.es>
2015-02-03 15:02:55 +0000

    temperature.js modified, Celcius to Farenheit conversion done

--- temperature.js
+++ temperature.js
@@ -1,21 +1,34 @@
 function calcular_temperatura(){
     
     var temperatura = temperatura_original.value;//guardamos el valor de la temperatura qe ha insertado el usuario
-    
+    var expresion_regular = /([+-]?\d+(?:\.\d+)?)\s*([CcFf])/;
+    var matching = temperatura.match(expresion_regular);
+    var mensaje_mostrado = "Error en los datos de entrada. Pruebe algo cómo se indica en el formato.";
     /*
     * Comprobamos que el valor introducido tiene el formato correcto, si no se muestra una alerta 
     */
-    if(temperatura.match(/([+-]?\d+(?:\.\d+)?)\s*([CcFf])/)){
+    if(matching){
+        var numero = matching[1];
+        var tipo_grado = matching[2];
         
+        switch (tipo_grado) {
+            case 'c' || 'C':
+                numero = numero * 1.8 + 32;
+                mensaje_mostrado = numero + "º Farenheit";
+                break;
+            
+            default:
+                // code
+        }
         temperatura_convertida.className = "alert alert-success"
         temperatura_convertida.style.display = "block"
-        temperatura_convertida.innerHTML = "Valor correcto";
+        temperatura_convertida.innerHTML = mensaje_mostrado;
         
     }else{
         
         temperatura_convertida.className = "alert alert-danger"
         temperatura_convertida.style.display = "block"
-        temperatura_convertida.innerHTML = "Error en los datos de entrada. Pruebe algo cómo se indica en el formato.";
+        temperatura_convertida.innerHTML = mensaje_mostrado;
    
     }
 }
