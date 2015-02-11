fb359a1be5e995faa619ffb500bfe0554f6e01ce
Victor Hernandez Perez <alu0100697032.edu.es>
2015-02-11 10:07:09 +0000

    Tests fixed

--- tests/tests.js
+++ tests/tests.js
@@ -2,13 +2,14 @@ var assert = chai.assert;
 
 suite('temperature', function(){
     test('32f = 0c', function(){
-       temperatura_original.value = "30f"; 
+       temperatura_original.value = "32f"; 
+       celcius.checked = true;
        calcular_temperatura();
        assert.deepEqual(temperatura_convertida.innerHTML, "0º Celcius")
     });
     test('5X = error', function() {
         temperatura_original.value = "5X";
         calcular_temperatura();
-        assert.match(temperatura_convertida.innerHTML, "Error en los datos de entrada. Pruebe algo cómo se indica en el formato.");
+        assert.deepEqual(temperatura_convertida.innerHTML, "Error en los datos de entrada. Pruebe algo cómo se indica en el formato.");
     });
 });
