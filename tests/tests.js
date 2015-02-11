4b933c0eb18d1e7592a709de4b3c02cec63fbae1
Victor Hernandez Perez <alu0100697032.edu.es>
2015-02-11 11:57:09 +0000

    Added more tests: celcius to...

--- tests/tests.js
+++ tests/tests.js
@@ -33,6 +33,38 @@ suite('Temperature converter', function(){
            assert.deepEqual(temperatura_convertida.innerHTML, "4º Réaumur")
         });
     });
+    suite('Celcius to...', function(){
+        test('0c = 0º Celcius', function(){
+           temperatura_original.value = "0c"; 
+           celcius.checked = true;
+           calcular_temperatura();
+           assert.deepEqual(temperatura_convertida.innerHTML, "0º Celcius")
+        });
+        test('-5c = 23º Farenheit', function(){
+           temperatura_original.value = "-5c"; 
+           farenheit.checked = true;
+           calcular_temperatura();
+           assert.deepEqual(temperatura_convertida.innerHTML, "23º Farenheit")
+        });
+        test('10c = 283.15º Kelvin', function(){
+           temperatura_original.value = "10c"; 
+           kelvin.checked = true;
+           calcular_temperatura();
+           assert.deepEqual(temperatura_convertida.innerHTML, "283.15º Kelvin")
+        });
+        test('-100c = 311.67º Rankine', function(){
+           temperatura_original.value = "-100c"; 
+           rankine.checked = true;
+           calcular_temperatura();
+           assert.deepEqual(temperatura_convertida.innerHTML, "311.67º Rankine")
+        });
+        test('2c = 1.6º Réaumur', function(){
+           temperatura_original.value = "2c"; 
+           reaumur.checked = true;
+           calcular_temperatura();
+           assert.deepEqual(temperatura_convertida.innerHTML, "1.6º Réaumur")
+        });
+    });
     test('5X = error', function() {
         temperatura_original.value = "5X";
         calcular_temperatura();
