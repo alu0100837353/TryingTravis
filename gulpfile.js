7ed5676a35292b25627bdc13d359a687c4d519ab
Victor Hernandez Perez <alu0100697032.edu.es>
2015-02-12 10:07:07 +0000

    Added some dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,19 @@ var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
 
 gulp.task('minify', function () {
-  gulp.src('temperature.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minified'));
+  gulp.src('js/temperature.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('minified'));
 
-  gulp.src('./index.html')
+  gulp.src('index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest('./minified/'))
 
-  gulp.src('./*.css')
+  gulp.src('css/normalize.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+   
+   gulp.src('css/styles.css')
    .pipe(minifyCSS({keepBreaks:true}))
    .pipe(gulp.dest('./minified/'))
 });
@@ -23,3 +27,5 @@ gulp.task('minify', function () {
 gulp.task('clean', function(cb) {
   del(['minified/*'], cb);
 });
+
+/*Tarea para ejecutar los tests*/
